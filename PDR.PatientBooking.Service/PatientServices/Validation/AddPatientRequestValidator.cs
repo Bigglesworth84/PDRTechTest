d642c3be716536e447277ab441042e13b6cd0f9a
Philip Webb <webbphil@hotmail.com>
2021-07-20 23:17:42 +0200

    Ticket 1: Fixed all failing tests in the solution.

--- PDR.PatientBooking.Service/PatientServices/Validation/AddPatientRequestValidator.cs
+++ PDR.PatientBooking.Service/PatientServices/Validation/AddPatientRequestValidator.cs
@@ -3,6 +3,7 @@ using PDR.PatientBooking.Service.PatientServices.Requests;
 using PDR.PatientBooking.Service.Validation;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace PDR.PatientBooking.Service.PatientServices.Validation
 {
@@ -20,6 +21,9 @@ namespace PDR.PatientBooking.Service.PatientServices.Validation
             var result = new PdrValidationResult(true);
 
             if (MissingRequiredFields(request, ref result))
+                return result;
+
+            if (InvalidEmailAddress(request, ref result))
                 return result;
 
             if (PatientAlreadyInDb(request, ref result))
@@ -43,6 +47,23 @@ namespace PDR.PatientBooking.Service.PatientServices.Validation
 
             if (string.IsNullOrEmpty(request.Email))
                 errors.Add("Email must be populated");
+
+            if (errors.Any())
+            {
+                result.PassedValidation = false;
+                result.Errors.AddRange(errors);
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool InvalidEmailAddress(AddPatientRequest request, ref PdrValidationResult result)
+        {
+            var errors = new List<string>();
+
+            if (!Regex.IsMatch(request.Email, @"^[\w-+\.]+@([\w-]+\.)+[\w-]{2,4}$"))
+                errors.Add("Email must be a valid email address");
 
             if (errors.Any())
             {
